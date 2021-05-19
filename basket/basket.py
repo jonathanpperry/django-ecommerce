511be6f6d3b30721a336dd0463daa1b4a9813d83
Jonathan Perry <jpper@umich.edu>
2021-05-19 14:34:37 +0900

    Implemented sum of all items in a view method and rendering value in basket page

--- basket/basket.py
+++ basket/basket.py
@@ -1,3 +1,5 @@
+from store.models import Product
+from decimal import Decimal
 
 
 class Basket():
@@ -23,8 +25,28 @@ class Basket():
 
         self.session.modified = True
 
+    def __iter__(self):
+        """-
+        Collect the product_id in the session data to query the database
+        and return products
+        """
+        product_ids = self.basket.keys()
+        products = Product.products.filter(id__in=product_ids)
+        basket = self.basket.copy()
+
+        for product in products:
+            basket[str(product.id)]['product'] = product
+
+        for item in basket.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['qty']
+            yield item
+
     def __len__(self):
         """
         Get the basket data and count the qty of items
         """
         return sum(item['qty'] for item in self.basket.values())
+
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['qty'] for item in self.basket.values())
