be09c477aef2f576e0fd40c7071647412a7c7b06
Jonathan Perry <jpper@umich.edu>
2021-05-19 17:01:44 +0900

    Wrote tests to cover basket/basket and basket/views underly code covered new files

--- store/tests/test_views.py
+++ store/tests/test_views.py
@@ -1,4 +1,6 @@
+from importlib import import_module
 
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.http import HttpRequest
 from django.test import Client, RequestFactory, TestCase
@@ -11,7 +13,6 @@ from store.views import product_all
 class TestViewResponses(TestCase):
     def setUp(self):
         self.c = Client()
-        self.factory = RequestFactory()
         User.objects.create(username='admin')
         Category.objects.create(name='django', slug='django')
         Product.objects.create(category_id=1, title='django beginners',
@@ -21,7 +22,7 @@ class TestViewResponses(TestCase):
         """
         Test allowed hosts
         """
-        response = self.c.get('/', HTTP_POST='noaddress.com')
+        response = self.c.get('/', HTTP_HOST='noaddress.com')
         self.assertEqual(response.status_code, 400)
         response = self.c.get('/', HTTP_POST='e-commercedjangoapp.herokuapp.com')
         self.assertEqual(response.status_code, 200)
@@ -44,17 +45,11 @@ class TestViewResponses(TestCase):
 
     def test_homepage_html(self):
         request = HttpRequest()
+        engine = import_module(settings.SESSION_ENGINE)
+        request.session = engine.SessionStore()
         response = product_all(request)
         html = response.content.decode('utf8')
-        # print(html)
-        self.assertIn('<title>BookStore</title>', html)
-        self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
-        self.assertEqual(response.status_code, 200)
 
-    def test_view_function(self):
-        request = self.factory.get('/django-beginners')
-        response = product_all(request)
-        html = response.content.decode('utf8')
         self.assertIn('<title>BookStore</title>', html)
         self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
         self.assertEqual(response.status_code, 200)
