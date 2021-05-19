18e2d561b1d2e1ec3f5622afaa8363d8f78821e0
Jonathan Perry <jpper@umich.edu>
2021-05-19 13:08:08 +0900

    Implemented add to cart functionality in session data

--- basket/basket.py
+++ basket/basket.py
@@ -10,5 +10,15 @@ class Basket():
         self.session = request.session
         basket = self.session.get('skey')
         if 'skey' not in request.session:
-            basket = self.session['skey'] = {'number': 123123123}
+            basket = self.session['skey'] = {}
         self.basket = basket
+
+    def add(self, product):
+        """
+        Adding and updating the users basket session data
+        """
+        product_id = product.id
+        if product_id not in self.basket:
+            self.basket[product_id] = {'price': str(product.price)}
+
+        self.session.modified = True
