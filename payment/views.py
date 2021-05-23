bbdc926d14de97f14f7765c257aa4e80ef2874f2
Jonathan Perry <jpper@umich.edu>
2021-05-23 16:34:59 +0900

    Finished implementing stripe elements section (4 hours)

--- payment/views.py
+++ payment/views.py
@@ -1,7 +1,24 @@
+import json
+
+import stripe
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from basket.basket import Basket
+
+
 @login_required
 def BasketView(request):
-    return render(request, 'payment/home.html')
+    basket = Basket(request)
+    total = str(basket.get_total_price())
+    total = total.replace('.', '')
+    total = int(total)
 
+    stripe.api_key = ''
+    intent = stripe.PaymentIntent.create(
+        amount=total,
+        currency='gbp',
+        metadata={'userid': request.user.id}
+    )
+
+    return render(request, 'payment/home.html', {'client_secret': intent.client_secret})
